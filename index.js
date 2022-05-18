0d565e287c1246d1c556a119e7c01e3edc2f1cc5
mjaumi <mjaumi2864@gmail.com>
2022-05-18 23:50:52 +0600

    POST API for adding task to database done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -8,10 +9,34 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// mongoDB connection here
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ttk1i.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+    await client.connect();
+
+    const todoCollection = client.db('todo').collection('tasks');
+
+    console.log('MongoDB Connected!');
+
+    //POST API for adding a task to the database
+    app.post('/task', async (req, res) => {
+        const task = req.body;
+        const addResult = await todoCollection.insertOne(task);
+        res.send(addResult);
+    });
+}
+
+run().catch(console.dir);
+
+// base API
 app.get('/', (req, res) => {
     res.send('TO-DO Server Running!!!');
 });
 
+// running port
 app.listen(port, () => {
     console.log('TO-DO server running on port', port);
 })
